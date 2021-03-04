9c9bb4e69b89381f7e1fcff2558cda9c63f0935e
Jesse <jesseweijapan@gmail.com>
2021-03-04 21:47:25 +0900

    Fix Languages html

--- src/components/Languages.js
+++ src/components/Languages.js
@@ -10,26 +10,28 @@ export default function Languages() {
     return (
         <>
             <TopHeader />
-            <div className="bg-white container">
-                <ul className="container">
-                    {data.languages.map(lang => {
-                        const id = lang.id;
-                        return (
-                            <li key={id} className="mb-5">
-                                <Link className="fs3" to={ `/lang/${id}` }>
-                                    <Button
-                                        label={ lang.name }
-                                        name=""
-                                        onClick={() => console.log('Hi')}
-                                    />
-                                </Link>
-                            </li>
-                        );
-                    }) }
-                    <li>
-                        <AddButton  />
-                    </li>
-                </ul>
+            <div className="container">
+                <div className="bg-white">
+                    <ul className="container">
+                        {data.languages.map(lang => {
+                            const id = lang.id;
+                            return (
+                                <li key={id} className="mb-5">
+                                    <Link className="fs3" to={ `/lang/${id}` }>
+                                        <Button
+                                            label={ lang.name }
+                                            name=""
+                                            onClick={() => console.log('Hi')}
+                                        />
+                                    </Link>
+                                </li>
+                            );
+                        }) }
+                        <li>
+                            <AddButton  />
+                        </li>
+                    </ul>
+                </div>
             </div>
         </>
     )
